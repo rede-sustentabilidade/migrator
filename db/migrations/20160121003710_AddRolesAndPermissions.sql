835521e793624cd827d3ea81df501da09ecdb3a9
Luiz Armesto <luiz.armesto@gmail.com>
2016-01-27 00:53:29 -0200

    Popula tabela roles com papeis pre estabelecidos

--- db/migrations/20160121003710_AddRolesAndPermissions.sql
+++ db/migrations/20160121003710_AddRolesAndPermissions.sql
@@ -21,13 +21,21 @@ ALTER TABLE ONLY roles ADD CONSTRAINT created_by_fkey FOREIGN KEY (created_by)
     REFERENCES afiliados(user_id) ON UPDATE CASCADE ON DELETE CASCADE;
 
 
-INSERT INTO rs.roles (id, level, name, description, superuser) VALUES (
-  1,
-  0,
-  'admin',
-  'Administrador',
-  TRUE
-);
+INSERT INTO rs.roles (id, level, name, description, superuser) VALUES (1, 0, 'admin', 'Administrador', TRUE);
+INSERT INTO rs.roles (id, level, name, description, superuser) VALUES (2, 1, 'coord_organizacao', 'Coordenador de Organização', FALSE);
+INSERT INTO rs.roles (id, level, name, description, superuser) VALUES (3, 1, 'coord_executiva', 'Coordenador da Executiva', FALSE);
+INSERT INTO rs.roles (id, level, name, description, superuser) VALUES (4, 1, 'coord_geral', 'Coordenador Geral', FALSE);
+INSERT INTO rs.roles (id, level, name, description, superuser) VALUES (5, 1, 'coord_formacao', 'Coordenador de Formação', FALSE);
+INSERT INTO rs.roles (id, level, name, description, superuser) VALUES (6, 1, 'coord_comunicacao', 'Coordenador de Comunicação', FALSE);
+INSERT INTO rs.roles (id, level, name, description, superuser) VALUES (7, 1, 'coord_acao_institucional', 'Coordenador de Ação Institucional', FALSE);
+INSERT INTO rs.roles (id, level, name, description, superuser) VALUES (8, 1, 'coord_politicas_publicas', 'Coordenador de Políticas Públicas', FALSE);
+INSERT INTO rs.roles (id, level, name, description, superuser) VALUES (9, 1, 'coord_movimentos_sociais', 'Coordenador de Movimentos Sociais', FALSE);
+INSERT INTO rs.roles (id, level, name, description, superuser) VALUES (10, 1, 'coord_ativismo', 'Coordenador de Ativismo', FALSE);
+INSERT INTO rs.roles (id, level, name, description, superuser) VALUES (11, 1, 'coord_relacoes_internacionais', 'Coordenador de ', FALSE);
+INSERT INTO rs.roles (id, level, name, description, superuser) VALUES (12, 1, 'coord_vogal_executiva', 'Coordenador de Vogal Executiva', FALSE);
+INSERT INTO rs.roles (id, level, name, description, superuser) VALUES (13, 100, 'filiado', 'Filiado', FALSE);
+INSERT INTO rs.roles (id, level, name, description, superuser) VALUES (14, 1000, 'usuario', 'Usuário Não Filiado', FALSE);
+INSERT INTO rs.roles (id, level, name, description, superuser) VALUES (15, 10000, 'anonimo', 'Usuário Anônimo', FALSE);
 
 
 CREATE TABLE IF NOT EXISTS rs.link_roles_afiliados (
